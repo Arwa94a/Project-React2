4590abe9711c9bf3eccd4ce91f4ec44cc8daec8c
SuaadAlgamdi <Suaad.Abduallh.abt.js@tuwaiq.edu.sa>
2021-11-22 08:57:30 +0300

    add

--- src/Pages/VidHtml.js
+++ src/Pages/VidHtml.js
@@ -11,6 +11,7 @@ function VidHtml() {
     
     <Row className="ms-5" >
     <Col>
+    <h2> Centering in Html</h2>
       <video width="500" height="500" controls>
         <source src={vidhtml2} type="video/mp4" />
        
@@ -18,16 +19,18 @@ function VidHtml() {
     <p class="text-center"> who to do centering align in html? The center tag is one of the HTML tags; when we use these tags in the document, it will align the paragraphs or words or any text, values it can be aligned with the center position of the web pages. In Html, these tags will use all the versions except Html5 instead of that CSS style have a property to align the elements with the center position. </p>
       </Col>
       <Col>
+      <h2> Margin in Html</h2>
       <video width="500" height="500" controls>
         <source src={vidhtml3} type="video/mp4"md="" />
         
       </video>
-     <p class="text-center"> who to Mardin in html and cssThe margin specifies a shorthand property for setting the margin properties in one </p>
+     <p class="text-center"> who to Margin in html and cssThe margin specifies a shorthand property for setting the margin properties in one </p>
       </Col>
       
        </Row>
        <Row className="ms-5">
       <Col>  
+      <h2> Index in HTML </h2>
       <video width="500" height="500" controls>
         <source src={vidhtml1} type="video/mp4" md="3" />
        
@@ -36,6 +39,7 @@ function VidHtml() {
       <p class="text-center">Index in html The index.html page is the most common name used for the default page shown on a website if no other page is specified when a visitor requests the site. In other words, index.html is the name used for the homepage of the website. Websites are built inside of directories on a web serve</p>
      </Col>
       <Col>
+      <h2> Html Callback </h2>
       <video width="500" height="500" controls>
         <source src={vidhtml1} type="video/mp4" />
          </video>
--- src/Pages/VidJs.js
+++ src/Pages/VidJs.js
@@ -10,14 +10,17 @@ function VidHtml() {
     <> 
     
     <Row className="ms-5" >
-    <Col>
-      <video width="500" height="500" controls>
+    <Col> 
+    <h2> Asynchronoust </h2>
+      <video width="500" height="500" controls> 
+      
         <source src={Asynchronous} type="video/mp4" />
        
       </video>
     <p class="text-center">How Does Async/Await Work in JavaScrip?Async means asynchronous. It allows a program to run a function without freezing the entire program. This is done using the Async/Await keyword. Async/Await makes it easier to write promises. The keyword ‘async’ before a function makes the function return a promise </p>
       </Col>
       <Col>
+      <h2> Getter And Sertter </h2>
       <video width="500" height="500" controls>
         <source src={GetterSetter} type="video/mp4"md="" />
         
@@ -27,7 +30,8 @@ function VidHtml() {
       
        </Row>
        <Row className="ms-5">
-      <Col>  
+      <Col> 
+      <h2> Optional Chaining </h2> 
       <video width="500" height="500" controls>
         <source src={OptionalChaining} type="video/mp4" md="3" />
        
@@ -36,6 +40,7 @@ function VidHtml() {
       <p class="text-center">Optional chaining, represented by ?. in JavaScript, is a new feature introduced in ES2020. Optional chaining changes the way properties are accessed from deeply nested objects. It fixes the problem of having to do multiple null checks when accessing a long chain of object properties in Java Script</p>
      </Col>
       <Col>
+       <h2>Object In Java Script</h2>
       <video width="500" height="500" controls>
         <source src={Oobject} type="video/mp4" />
          </video>
